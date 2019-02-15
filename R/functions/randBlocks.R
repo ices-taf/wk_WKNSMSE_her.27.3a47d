046b4f5475f83caf1c61a5c8edfc7cba3c099424
Benoit Berges <benoit.berges@wur.nl>
2019-02-15 13:19:23 +0100

    update NSAS

--- R/functions/randBlocks.R
+++ R/functions/randBlocks.R
@@ -4,10 +4,10 @@ randBlocks <- function(fecYears,projPeriod,nits){
   
   saveBlcks_nYears     <- matrix(NA,nrow=nits,ncol=nyrs)
   saveBlcks_startYear  <- matrix(NA,nrow=nits,ncol=nyrs)
+  saveBlcks_rev        <- matrix(NA,nrow=nits,ncol=nyrs)
   
-  # generate random samples for the number of years
   for(idxIter in 1:nits){
-    saveBlcks_nYears[idxIter,] <- randNums(nyrs,1,length(fecYears)-1,nyrs)
+    saveBlcks_nYears[idxIter,] <- randNums(nyrs,1,length(fecYears)-3,nyrs)
   }
   
   # randomize the starting years
@@ -16,9 +16,18 @@ randBlocks <- function(fecYears,projPeriod,nits){
                                    !is.na(saveBlcks_nYears[idxIter,])] # select only non NaN values
     # loop on non NaN values
     for(idxYear in 1:length(nYearsTemp)){
-      saveBlcks_startYear[idxIter,idxYear] <- round(runif(n = 1, 
-                                                          min=min(an(fecYears)), 
-                                                          max=max(an(fecYears))-nYearsTemp[idxYear])) # random number for the starting year
+      
+      if(round(runif(1,0,1)) == 0){
+        saveBlcks_startYear[idxIter,idxYear] <- round(runif(n = 1, 
+                                                            min=min(an(fecYears))-0.49, 
+                                                            max=(max(an(fecYears))-nYearsTemp[idxYear])+0.49)) # random number for the starting year
+        saveBlcks_rev[idxIter,idxYear] <- 0
+      }else{
+        saveBlcks_startYear[idxIter,idxYear] <- round(runif(n = 1, 
+                                                            min=(min(an(fecYears))+nYearsTemp[idxYear])-0.49, 
+                                                            max=max(an(fecYears))+0.49)) # random number for the starting year
+        saveBlcks_rev[idxIter,idxYear] <- 1
+      }
     }
   }
   
@@ -28,19 +37,23 @@ randBlocks <- function(fecYears,projPeriod,nits){
   # simply loop on start years and number of years with a randomizer for a possible reverse order 
   for(idxIter in 1:nits){
     for(idxYear in 1:nyrs){
-      tempVal <- c(saveBlcks_startYear[idxIter,idxYear],
-                   saveBlcks_startYear[idxIter,idxYear] + saveBlcks_nYears[idxIter,idxYear])
-      tempVal[2] <- tempVal[2]-1
-      
-      if(round(runif(1,0,1)) == 0) # randomize the inverse orderind of the years
-        tempVal <- tempVal[2:1]
-      
-      saveBlcksYrs[idxIter,idxYear,"start"] <- tempVal[1]
-      saveBlcksYrs[idxIter,idxYear,"stop"]  <- tempVal[2]
+      if(!is.na(saveBlcks_startYear[idxIter,idxYear])){
+        if(saveBlcks_rev[idxIter,idxYear] == 1){ # reverse
+          tempVal <- c( saveBlcks_startYear[idxIter,idxYear],
+                        saveBlcks_startYear[idxIter,idxYear] - saveBlcks_nYears[idxIter,idxYear])
+          tempVal[2] <- tempVal[2]+1
+        }else{ # normal order
+          tempVal <- c(saveBlcks_startYear[idxIter,idxYear],
+                       saveBlcks_startYear[idxIter,idxYear] + saveBlcks_nYears[idxIter,idxYear])
+          tempVal[2] <- tempVal[2]-1
+        }
+        
+        saveBlcksYrs[idxIter,idxYear,"start"] <- tempVal[1]
+        saveBlcksYrs[idxIter,idxYear,"stop"]  <- tempVal[2]
+      }
     }
   }
   
-  #-Create year strings and bind them together
   yrStrngsIter <- apply(saveBlcksYrs,1,function(x){mapply(seq,from=na.omit(x[,1]),to=na.omit(x[,2]))})
   
   # if each sampled block was the same length, it creates a matrix instead of a list, gives an error below.
