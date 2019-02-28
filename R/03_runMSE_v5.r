bf37dbf60e27fcb40f982b7b6926fbacca092f89
nielshintzen <niels.hintzen@gmail.com>
2019-02-28 13:42:57 +0100

    update FHCR with nits iters

--- R/03_runMSE_v5.r
+++ R/03_runMSE_v5.r
@@ -22,7 +22,7 @@
 
 
 args=(commandArgs(TRUE))
-#args <- 'ftar=0.24_btrig=1.8e6_HCR=1_IAV=0_BB=0'
+#args <- 'ftar=0.24_btrig=1.4e6_HCR=1_IAV=0_BB=0'
 args    <- strsplit(args,"_")
 ftarget <- as.numeric(substr(args[[1]][1],6,9))
 btrigger<- as.numeric(substr(args[[1]][2],7,11))
@@ -224,8 +224,8 @@ if(newUptakes){
   load(file.path(outPath,paste0("SplitUptakes",nits,".RData")))
 }
 CATCH                     <- TAC
-FHCR                      <- FLQuant(NA, dimnames=c(age=0:8,year=projPeriod,unit="unique",season="all",area="unique",iter=1:nits))
-SSBHCR                    <- FLQuant(NA, dimnames=c(age="all",year=projPeriod,unit="unique",season=c("FcY","CtY"),area="unique",iter=1:nits))
+FHCR                      <- FLQuant(NA, dimnames=list(age=0:8,year=projPeriod,unit="unique",season="all",area="unique",iter=1:nits))
+SSBHCR                    <- FLQuant(NA, dimnames=list(age="all",year=projPeriod,unit="unique",season=c("FcY","CtY"),area="unique",iter=1:nits))
 
 
 #------------------------------------------------------------------------------#
